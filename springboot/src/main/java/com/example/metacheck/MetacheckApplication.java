ee589b9555ff1de483abb311f384e666e5749601
snowbeach0102 <poby262513@gmail.com>
2026-09-25 16:21:36 +0900

    스프링부트까지 설정

---
+++ springboot/src/main/java/com/example/metacheck/MetacheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.metacheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetacheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetacheckApplication.class, args);
+	}
+
+}
---
+++ springboot/src/test/java/com/example/metacheck/MetacheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.metacheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetacheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
